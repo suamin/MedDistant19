9b247c34f28b8ef395b6208860448601739132d0
Saadullah Amin <saadullahamin@gmail.com>
2022-01-05 00:30:17 +0100

    added benchmark script

---
+++ benchmark/download_meddistant19.sh
@@ -0,0 +1,7 @@
+mkdir meddistant19
+
+wget --content-disposition https://cloud.dfki.de/owncloud/index.php/s/MQoMJtdDnCKEbDa/download -P meddistant19/
+wget --content-disposition https://cloud.dfki.de/owncloud/index.php/s/mtekKoQZ4nK7yEp/download -P meddistant19/
+wget --content-disposition https://cloud.dfki.de/owncloud/index.php/s/miDJDpwo2NkWDgg/download -P meddistant19/
+wget --content-disposition https://cloud.dfki.de/owncloud/index.php/s/9SDnD77dkokNrsc/download -P meddistant19/
+
