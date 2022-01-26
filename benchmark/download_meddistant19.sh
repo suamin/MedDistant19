07fea4ee3c9f059d1d77bcc9e5f8e16d13e09c55
Saadullah Amin <saadullahamin@gmail.com>
2022-01-26 17:22:10 +0100

    update

--- benchmark/download_meddistant19.sh
+++ benchmark/download_meddistant19.sh
@@ -1,7 +1,6 @@
-mkdir meddistant19
+mkdir med_distant19
 
-wget --content-disposition https://cloud.dfki.de/owncloud/index.php/s/MQoMJtdDnCKEbDa/download -P meddistant19/
-wget --content-disposition https://cloud.dfki.de/owncloud/index.php/s/mtekKoQZ4nK7yEp/download -P meddistant19/
-wget --content-disposition https://cloud.dfki.de/owncloud/index.php/s/miDJDpwo2NkWDgg/download -P meddistant19/
-wget --content-disposition https://cloud.dfki.de/owncloud/index.php/s/9SDnD77dkokNrsc/download -P meddistant19/
-
+wget --content-disposition https://cloud.dfki.de/owncloud/index.php/s/MQoMJtdDnCKEbDa/download -P med_distant19/
+wget --content-disposition https://cloud.dfki.de/owncloud/index.php/s/mtekKoQZ4nK7yEp/download -P med_distant19/
+wget --content-disposition https://cloud.dfki.de/owncloud/index.php/s/miDJDpwo2NkWDgg/download -P med_distant19/
+wget --content-disposition https://cloud.dfki.de/owncloud/index.php/s/9SDnD77dkokNrsc/download -P med_distant19/
